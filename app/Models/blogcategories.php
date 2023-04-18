a05e061fcf2bf28bc50f44e41df138be0fb37155
Guyatuu <gsubez@gmail.com>
2023-04-18 20:17:21 +0300

    first commit

---
+++ app/Models/blogcategories.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class blogcategories extends Model
+{
+    use HasFactory;
+    protected $table = 'blog-categories';
+    protected $fillable = [
+        'name'    
+    ];
+    public $timestamps = false;
+}
